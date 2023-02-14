4ab0052bea40169f58d7a0bdeba653507b80299e
Lyudmila Churina <notyourbaryga@gmail.com>
2023-02-15 02:54:11 +0500

    сука)))))))))

---
+++ bot/porn_api.py
@@ -0,0 +1,13 @@
+from pornhub_api import PornhubApi
+
+#это пока не надо
+api = PornhubApi()
+
+#временная функция, надо заменить на нормальную
+def getporn_from_request(s: str) -> str:
+    return('суперпорно с конями и ' + s)
+
+def getporn_from_category(s: str) -> str:
+    pass
+
+#че блять?
