87e207c928488c593688a0038b0ffa41e0210fe2
Ebalo Off <spirin300@gmail.com>
2023-02-15 04:19:00 +0300

    я тупица)))

--- bot/porn_api.py
+++ bot/porn_api.py
@@ -1,13 +1,28 @@
+import requests
 from pornhub_api import PornhubApi
+from bs4 import BeautifulSoup as BS
+#с-76 gay sex(https://www.pornhub.com/video?c=76) c-80 orgy(https://www.pornhub.com/video?c=80)
+#  c-17 ebony(https://www.pornhub.com/video?c=17) c-1 asia(https://www.pornhub.com/video?c=1)
+
+
 
 #это пока не надо
 api = PornhubApi()
 
 #временная функция, надо заменить на нормальную
 def getporn_from_request(s: str) -> str:
+    
     return('суперпорно с конями и ' + s)
 
 def getporn_from_category(s: str) -> str:
+   # category = dict ([('gay_sex', 1), ('orgy', 2), ('ebony', 3), ('asia', 4)])
+    
+    
+    
+    #category = urllib.request.urlopen("https://www.pornhub.com/video?c=76")    <------- (stackoverflow) будет, вроде как, сёрчить в данной категории на одной странице 
+   # soup = BeautifulSoup(html_page, "html.parser")
+    #for link in soup.findAll('a'):
+      #  print(link.get('href'))
     pass
 
-#че блять?
+#че блять?       бл я тупой          мой мозк поплыл при попытке присвоить ссылки значениям 1,2,3,4  мне нужна твоя помощьб мома (つ﹏⊂)
